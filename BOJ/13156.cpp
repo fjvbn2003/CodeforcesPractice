dc4ca28b875ccd5742ce8ad315b61d5ecbad5fa2
fjvbn2003 <fjvbn2003@gmail.com>
2023-01-23 23:53:33 +0900

    ADD BOJ 13156

---
+++ BOJ/13156.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int c, m;
+    cin>> c>>m;
+    for(int i=0; i<m; i++){
+        for(int j=0; j<c; j++){
+            
+        }
+    }
+
+}
