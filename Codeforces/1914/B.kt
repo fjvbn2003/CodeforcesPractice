0d705d3a31d73b4a704d8ef48dc055bcd857a12a
fjvbn2003 <fjvbn2003@gmail.com>
2024-01-02 19:14:51 +0900

    ADD B.kt

---
+++ Codeforces/1914/B.kt
@@ -0,0 +1,10 @@
+//ttps://codeforces.com/problemset/problem/1914/B
+
+/*
+youngjukim@youngjukim-Dell-G15-5511:~/work/CodeforcesPractice/Codeforces/1914$ kotlinc B.kt -include-runtime -d B.jar
+youngjukim@youngjukim-Dell-G15-5511:~/work/CodeforcesPractice/Codeforces/1914$ java -jar B.jar 
+Hello, World!
+ */
+fun main() {
+    println("Hello, World!")
+}
