08c5f9aa4a36c545c69558e40b67112df849b76f
fjvbn2003 <fjvbn2003@gmail.com>
2024-01-05 05:14:34 +0900

    ADD kotlin compile & run alias

--- Codeforces/1914/B.kt
+++ Codeforces/1914/B.kt
@@ -4,7 +4,15 @@
 youngjukim@youngjukim-Dell-G15-5511:~/work/CodeforcesPractice/Codeforces/1914$ kotlinc B.kt -include-runtime -d B.jar
 youngjukim@youngjukim-Dell-G15-5511:~/work/CodeforcesPractice/Codeforces/1914$ java -jar B.jar 
 Hello, World!
+
+$ alias runkt='f() { kotlinc $1.kt -include-runtime -d $1.jar && java -jar $1.jar; unset -f f; }; f'
  */
+
+
 fun main() {
     println("Hello, World!")
+  val (a, b) = readLine()!!.split(' ').map(String::toInt)
+  println(a + b)
 }
+
+
