c109d35626c9e64b9d99f97fcd781700efd08482
fjvbn2003 <fjvbn2003@gmail.com>
2024-01-11 02:27:19 +0900

    ADD 2D array example

--- Codeforces/1905/A.kt
+++ Codeforces/1905/A.kt
@@ -1,12 +1,27 @@
 // https://codeforces.com/contest/1905/problem/A
 
-
-fun main(){
+fun main() {
     var t = readLine()!!.toInt()
-    while(t>0){
-        var (n, m) = readLine()!!.split(" ").map{it.toInt()}
-        println(if(n>m) n else m)
+    while (t > 0) {
+        var (n, m) = readLine()!!.split(" ").map { it.toInt() }
+        println(if (n > m) n else m)
         t--
-    }
 
+        // Define a 2D array of integers with 3 rows and 4 columns
+        val twoDArray = Array(3) { IntArray(4) }
+
+        // Initialize the array with values
+        for (i in twoDArray.indices) {
+            for (j in twoDArray[i].indices) {
+                twoDArray[i][j] = i + j // Just an example; you can initialize it as needed
+            }
+        }
+
+        for (i in twoDArray.indices) {
+            for (j in twoDArray[i].indices) {
+                print("${twoDArray[i][j]} ")
+            }
+            println()
+        }
+    }
 }
